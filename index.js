066cc3dfbe142bf53800a6ec553326b58cb8da7f
Joseph Smith <joseph.smith@worldfirst.com>
2015-04-01 09:37:09 +0000

    Broadcast message to all connected clients

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ io.on('connection', function(socket){
         console.log('user disconnected');
     });
     socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
         console.log('message: ' + msg);
     });
 });
